983912a8eb7ddfab86d332365545900b3519effe
Brendan Ind <62707120+wycliffe-BI@users.noreply.github.com>
2020-07-08 22:30:00 +0100

    more image import messing

--- importing-photos.py
+++ importing-photos.py
@@ -7,7 +7,7 @@ import cv2 as cv
 import sys
 
 # img is essentially a big matrix, woooow
-img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+img = cv.imread("starry_night.jpg")
 
 if img is None:  # i.e. no image
     sys.exit("Couldn't read the image")
@@ -16,4 +16,5 @@ cv.imshow("Display window", img)
 k = cv.waitKey(0)  # parameter is how long program should wait for user input (in this case 0 is forever)
 
 if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+    cv.imwrite("starry_night.png", img)  # This writes the file back to the dir, i.e. it will arrive in .png format in
+    # the directory of this project
