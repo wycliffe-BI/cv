6c1162b84c6b61b0cc9b0771b235b118a6c6b8eb
Brendan Ind <62707120+wycliffe-BI@users.noreply.github.com>
2020-07-08 22:21:11 +0100

    deleted yeet, rip.

--- importing-photos.py
+++ importing-photos.py
@@ -2,14 +2,18 @@
 # Brendan Ind 2020
 # DWTFUL Licence (do what the **** you like)
 
+# Import stuff
 import cv2 as cv
 import sys
+
+# img is essentially a big matrix, woooow
 img = cv.imread(cv.samples.findFile("starry_night.jpg"))
 
-if img is None:
+if img is None:  # i.e. no image
     sys.exit("Couldn't read the image")
 
 cv.imshow("Display window", img)
+k = cv.waitKey(0)  # parameter is how long program should wait for user input (in this case 0 is forever)
 
 if k == ord("s"):
     cv.imwrite("starry_night.png", img)
