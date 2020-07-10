8cc8348b981416675df3c84e327994d587d6af91
Brendan Ind <62707120+wycliffe-BI@users.noreply.github.com>
2020-07-10 15:49:03 +0100

    added file to xplore image channel splitting matrices

--- basic-image-operations.py
+++ basic-image-operations.py
@@ -12,5 +12,48 @@
 
 import numpy as np
 import cv2 as cv
+from time import sleep
 
 # Load a colour image:
+img = cv.imread('messi5.jpg')
+
+
+def row_by_row(img=img):
+    # We can access a pixel value by its row and column coordinates
+    px = img[100, 100]
+    print(px)
+    print("Returned [157, 166, 200]?")
+
+
+def only_blue(img=img):
+    # Access only the blue pixels
+    blue = img[100, 100, 0]  # bgr
+    print(blue)
+    print("returned 157?")
+
+
+def modify_pixels(img=img):
+    img[100, 100] = [255, 255, 255]
+    print(img[100, 100])
+    print("Should return [255, 255, 255]")
+
+
+print("Size of image:")
+print(img.size)
+sleep(1)
+
+print("dtype is: (uint8 hopefully...)")
+print(img.dtype)
+sleep(1)
+
+# What we could do is cut a ROI (region of interest) and use matrix transformations to move it in the image.
+# Say our image of mess has the ball at:
+
+ball = img[280:340, 330:390]  # ball is in this region
+
+# then let:
+img[273:333, 100:160] = ball  # Replace pixels in the new region with the ball pixels
+
+cv.imshow("Display window", img)
+
+k = cv.waitKey(0)  # parameter is how long program should wait for user input (in this case 0 is forever)
---
+++ split-image-channels.py
@@ -0,0 +1,9 @@
+# This is a testing py file for splitting colour channels with openCV.
+# Brendan Ind 2020
+# DWTFUL Licence (do what the **** you like)
+
+import cv2 as cv
+import numpy as np
+
+# We are gonna split an img into its three channels, bgr.
+
