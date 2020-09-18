e3df7c8b44a7a07ff6dd9af8f32d9be580e107ee
wycliffeBI <indb@wycliffe.co.uk>
2020-09-18 14:08:11 +0100

    Added a keras test document and did more on main interface

--- user-interface.py
+++ user-interface.py
@@ -1,38 +1,54 @@
 # Brendan Ind
 # cv Project user interface
 
-from tkinter import *
+import tkinter as tk
 
-if __name__ = "__main__":
-    # On the first run we do this:
-    startup()
 
 def login():
     print("Login page")
+
 
 def startup():
     print("Startup")
 
 
+if __name__ == "__main__":
+    # On the first run we do this:
+    startup()
+
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.passwordBox = tk.Entry()
+        self.usernameBox = tk.Entry()
         self.master = master
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.login = tk.Button(self)
+        self.login["text"] = "LOGIN TO\nSERVER"
+        self.login["command"] = self.login
+        self.login.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+        self.passwordBox.pack()
+        self.passwordBox["text"] = "Enter Password"
+
+        self.usernameBox.pack()
+        self.usernameBox["text"] = "Enter Username"
+        self.usernameBox.bind("<Key-Return>", print("Hello"))
+
+    def login(self):
+        print("login to server button pressed")
+
+    def nextBox(self, event):  # Event is arg of event to switch focus to
+        event.focus_set()
+
 
 root = tk.Tk()
 app = Application(master=root)
