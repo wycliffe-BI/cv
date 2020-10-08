6af9db2c93c9710de7412dee3220392c25d057ce
wycliffeBI <indb@wycliffe.co.uk>
2020-10-08 18:18:52 +0100

    broken stuff trying to function-ise the code from azure

--- azure-face/face-detection.py
+++ azure-face/face-detection.py
@@ -1,6 +1,7 @@
 ## Brendan Ind
 ## Azure face detection
 
+## Imports:
 import asyncio
 import io
 import glob
@@ -16,34 +17,25 @@ from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
 
+
+## Set the keys for accessing azure cloud applications, set these on CONTROL PANEL on local machine, see readme for more.
 KEY = os.environ['FACE_SUBSCRIPTION_KEY']
 ENDPOINT = os.environ['FACE_ENDPOINT']
 
 # Create an authenticated FaceClient.
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 
-# Detect a face in an image that contains a single face
-single_face_image_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
-single_image_name = os.path.basename(single_face_image_url)
-detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
-if not detected_faces:
-    raise Exception('No face detected from image {}'.format(single_image_name))
 
-# Display the detected face ID in the first single-face image.
-# Face IDs are used for comparison to faces (their IDs) detected in other images.
-print('Detected face ID from', single_image_name, ':')
-for face in detected_faces: print (face.face_id)
-print()
+def detectFace(face_client, url):
+    image_name = os.path.basename(url)
+    detected_faces = face.client.face.detect_with_url(url=url)
+    if not detected_faces:
+        raise Exception('No face detected from image {}'.format(image_name))
+    print('Detected face ID from', single_image_name, ':')
+    for face in detected_faces:
+        print(face.face_id)
+    return detected_faces
 
-# Save this ID for use in Find Similar
-first_image_face_ID = detected_faces[0].face_id
-
-# Detect a face in an image that contains a single face
-single_face_image_url = 'https://cdn.cnn.com/cnnnext/dam/assets/201002004307-melania-trump-file-may-25-2020-exlarge-169.jpg'
-single_image_name = os.path.basename(single_face_image_url)
-detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
-if not detected_faces:
-    raise Exception('No face detected from image {}'.format(single_image_name))
 
 # Convert width height to a point in a rectangle
 def getRectangle(faceDictionary):
@@ -61,6 +53,8 @@ response = requests.get(single_face_image_url)
 img = Image.open(BytesIO(response.content))
 
 # For each face returned use the face rectangle and draw a red box.
+
+def drawRect
 print('Drawing rectangle around face... see popup for results.')
 draw = ImageDraw.Draw(img)
 for face in detected_faces:
