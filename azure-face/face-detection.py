b10c125f5ece04d4f31fa1280900fae87460f758
wycliffeBI <indb@wycliffe.co.uk>
2020-10-08 18:12:29 +0100

    GOT IT WORKING!!!! Trump and Melinda :)

--- azure-face/face-detection.py
+++ azure-face/face-detection.py
@@ -22,18 +22,49 @@ ENDPOINT = os.environ['FACE_ENDPOINT']
 # Create an authenticated FaceClient.
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 
-# # Detect a face in an image that contains a single face
-# single_face_image_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
-# single_image_name = os.path.basename(single_face_image_url)
-# detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
-# if not detected_faces:
-#     raise Exception('No face detected from image {}'.format(single_image_name))
-#
-# # Display the detected face ID in the first single-face image.
-# # Face IDs are used for comparison to faces (their IDs) detected in other images.
-# print('Detected face ID from', single_image_name, ':')
-# for face in detected_faces: print (face.face_id)
-# print()
-#
-# # Save this ID for use in Find Similar
-# first_image_face_ID = detected_faces[0].face_id
+# Detect a face in an image that contains a single face
+single_face_image_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
+single_image_name = os.path.basename(single_face_image_url)
+detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
+if not detected_faces:
+    raise Exception('No face detected from image {}'.format(single_image_name))
+
+# Display the detected face ID in the first single-face image.
+# Face IDs are used for comparison to faces (their IDs) detected in other images.
+print('Detected face ID from', single_image_name, ':')
+for face in detected_faces: print (face.face_id)
+print()
+
+# Save this ID for use in Find Similar
+first_image_face_ID = detected_faces[0].face_id
+
+# Detect a face in an image that contains a single face
+single_face_image_url = 'https://cdn.cnn.com/cnnnext/dam/assets/201002004307-melania-trump-file-may-25-2020-exlarge-169.jpg'
+single_image_name = os.path.basename(single_face_image_url)
+detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
+if not detected_faces:
+    raise Exception('No face detected from image {}'.format(single_image_name))
+
+# Convert width height to a point in a rectangle
+def getRectangle(faceDictionary):
+    rect = faceDictionary.face_rectangle
+    left = rect.left
+    top = rect.top
+    right = left + rect.width
+    bottom = top + rect.height
+
+    return ((left, top), (right, bottom))
+
+
+# Download the image from the url
+response = requests.get(single_face_image_url)
+img = Image.open(BytesIO(response.content))
+
+# For each face returned use the face rectangle and draw a red box.
+print('Drawing rectangle around face... see popup for results.')
+draw = ImageDraw.Draw(img)
+for face in detected_faces:
+    draw.rectangle(getRectangle(face), outline='red')
+
+# Display the image in the users default image browser.
+img.show()
